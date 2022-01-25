32484c9e9b4903019e1425b30ea5837faad3c33d
Taewoongjung <aipooh8882@naver.com>
2022-01-25 19:41:01 +0900

    added some contents in distribution function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -14,16 +14,23 @@ contract Lottery {
     mapping (uint256 => BetInfo) private _bets;
 
     /* public으로 만들게 되면 자동으로 getter를 만들어준다. 그래서 스마트 컨트랙트 외부에서 owner값을 확인할 수 있게되는 것이다. */
-    address public owner;
+    address payable public owner;
 
     uint256 constant internal BLOCK_LIMIT = 256;
     uint256 constant internal BET_BLOCK_INTERVAL = 3; // 블록들 간의 간격
     uint256 constant internal BET_AMOUNT = 5 * 10 ** 15; // 0.005ETH
     uint256 private _pot;
 
+    bool private mode = false; // false : use answer for test, true : real block hash
+    bytes32 public answerForTest;
+
     enum BlockStatus {Checkable, NotRevealed, BlockLimitPassed}
     enum BettingResult{Fail, Win, Draw}
     event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+    event WIN(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event FAIL(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event DRAW(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event REFUND(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
 
     /* 
         스마트 컨트랙트가 생성이 될 때(배포가 될 때) 가장 처음 실행되는 함수인데, 
@@ -58,22 +65,51 @@ contract Lottery {
         return true;
     }
         // save the bet to the queue
-
+    /**
+     * @dev 베팅 결과값을 확인 하고 팟머니를 분배한다.
+     * 정답 실패: 팟머니 축척, 정답 맞춤: 팟머니 획득, 한글자 맞춤 or 정답 확인 불가: 베팅 금액만 획득
+    */
     // Distribute - 베팅을 하면 정답을 맞춘 사람에게는 돈을 돌려주고 아니면 돈을 pot에 갖는 시스템
     function distribute() public {
         uint256 cur;
+        uint256 transferAmount;
+
         BetInfo memory b;
         BlockStatus currentBlockStatus;
+        BettingResult currentBettingResult;
+
         for(cur=_head;cur<_tail;cur++) {
             b = _bets[cur];
             currentBlockStatus = getBlockStatus((b.answerBlockNumber));
-
+            bytes32 answerBlockHash = getAnswerBlockHash(b.answerBlockNumber);
             if(currentBlockStatus == BlockStatus.Checkable) {
+                currentBettingResult = isMatch(b.challenges, answerBlockHash);
                 // if win, bettor gets pot
-
+                if(currentBettingResult == BettingResult.Win) {
+                    // transfer pot
+                    transferAmount = transferAfterPayingFee(b.bettor, _pot + BET_AMOUNT);
+                    
+                    // pot = 0
+                    _pot = 0;
+                    
+                    // emit event(WIN)
+                    emit WIN(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
                 // if fail bettor's money goes pot
-
+                if(currentBettingResult == BettingResult.Fail) {
+                    // pot = pot + BET_AMOUNT
+                    _pot += BET_AMOUNT;
+                    // emit FAIL
+                    emit FAIL(cur, b.bettor, 0, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
                 // if draw, refund bettor's money
+                if(currentBettingResult == BettingResult.Fail) {
+                    // transfer only BET_AMOUNT
+                    transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+
+                    // emit DRAW
+                    emit FAIL(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
             }
 
             if(currentBlockStatus == BlockStatus.NotRevealed) {
@@ -81,11 +117,36 @@ contract Lottery {
             }
 
             if(currentBlockStatus == BlockStatus.BlockLimitPassed) {
-                
+                // refund
+                transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+                //emit refund
+                emit REFUND(cur, b.bettor, transferAmount, b.challenges, b.answerBlockNumber);
             }
             popBet(cur);
         }
-    }    
+        _head = cur;
+    }
+    function transferAfterPayingFee(address payable addr, uint256 amount) internal returns (uint256) {
+        uint256 fee = 0;
+        uint256 amountWithoutFee = amount - fee;
+
+        // transfer to addr
+        addr.transfer(amountWithoutFee);
+        // transfer to owner
+        owner.transfer(fee);
+
+        // call, send, transfer
+        
+        return amountWithoutFee;
+    }
+    function setAnswerForTest(bytes32 answer) public returns (bool result) {
+        require(msg.sender == owner, "Only owner can set the answer for test mode");
+        answerForTest = answer;
+        return true;
+    }
+    function getAnswerBlockHash(uint256 answerBlockNumber) internal view returns (bytes32 answer) {
+        return mode ? blockhash(answerBlockNumber) : answerForTest;
+    }
     /**
      * @dev 베팅금지와 정답을 확인한다.
      * @param challenges 베팅 금지
